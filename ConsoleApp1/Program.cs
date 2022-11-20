8fdd5723adc513186d5b347a77018060aaf82334
Xelgo <slavin69@list.ru>
2022-11-20 14:51:42 +0400

    DS

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ class Program
         var cards = new Cards();
         InitGame(out var players);
 
-        //GA TEST LOL'D LOL'D2
+        //GA TEST LOL'D LOL'D2 DS2
 
         for (int i = 0; i < 150; i++)
         {
