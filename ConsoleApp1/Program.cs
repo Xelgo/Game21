f86cc6c491addf4cf71a43d1224ade4858ed52ff
Xelgo <slavin69@list.ru>
2022-11-20 14:52:44 +0400

    SDAD

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ class Program
         InitGame(out var players);
 
         //GA TEST LOL'D LOL'D2 DS2 SDG
-
+        //SDASDASD
         for (int i = 0; i < 150; i++)
         {
             var c = cards.GetCard();
