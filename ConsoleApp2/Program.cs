baba0155456b0ff748c6953a2d793c48486a27f2
user <user@WIN-13L6GHNV8T3>
2022-11-20 13:31:06 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int d = 2;
+            d++;
+            Console.WriteLine(d);
+
+
+            
+
+        }
+    }
+}
